1b5453b6bf8fc458d6fb864effc82e7a904deaba
viswa s <viswanathan.s@ericsson.com>
2020-11-15 21:35:23 +0530

    config map and sexrets

--- Fortune/fortune_args/fortuneloop.sh
+++ Fortune/fortune_args/fortuneloop.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 trap "exit" SIGINT
+INTERVAL=$1
+echo Configured to generate fortune every $INTERVAL seconds
+mkdir -p /var/htdocs
 while :
 do
   echo $(date) Writing fortune to /var/htdocs/index.html
   /usr/games/fortune > /var/htdocs/index.html
-  sleep 10
+  sleep $INTERVAL
 done
--- Fortune/fortune_env/fortuneloop.sh
+++ Fortune/fortune_env/fortuneloop.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 trap "exit" SIGINT
+echo Configured to generate fortune every $INTERVAL seconds
+mkdir -p /var/htdocs
 while :
 do
   echo $(date) Writing fortune to /var/htdocs/index.html
   /usr/games/fortune > /var/htdocs/index.html
-  sleep 10
+  sleep $INTERVAL
 done
